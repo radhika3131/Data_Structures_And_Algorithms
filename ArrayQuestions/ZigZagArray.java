5334c7e294ff3848c3a4627b7325e083b27f916e
Radhika Ramsen <102825662+radhika3131@users.noreply.github.com>
2023-06-26 10:38:45 -0700

    Add files via upload

---
+++ ArrayQuestions/ZigZagArray.java
@@ -0,0 +1,31 @@
+package com.dataStructures.ArrayQuestions;
+
+public class ZigZagArray {
+    public static void main(String[] args) {
+        int arr[] = {4,3,7,8,6,2,1};
+        boolean flag = true;
+        int temp = 0;
+        for(int i = 0; i<arr.length-1; i++){
+            if(flag){
+                if(arr[i]>arr[i+1]){
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+            else{ // flag is false
+                if(arr[i]<arr[i+1]){
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+            // toggle flag
+            flag = !flag;
+        }
+        System.out.println("Zig Zag Array....");
+        for(int i : arr){
+            System.out.println(i);
+        }
+    }
+}
